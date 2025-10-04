351b0f6e059d6e8c8582fc3abb2d8640ca174b9e
rohitt-nait <rohitt@nait.ca>
2025-10-03 18:47:51 -0600

    Practice exercise that is Day-7 is pushed by Rohit T

--- Practice-exercises/src/main.js
+++ Practice-exercises/src/main.js
@@ -1,3 +1,5 @@
+//This is old piece of functionality
+/*
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
@@ -22,5 +24,54 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+*/
+console.log('This is the step-1 after entering to the main.js block')
+const card_container=document.querySelector('.card')
+let input
+input= prompt('Please enter a label','JavaScript as Default')
 
-    
+//displaying the user input on the console.
+console.log(input);
+
+//Conditinal statement will be used to cover the objective.
+if(input===null)
+{
+  console.log('On clicking CANCEL button, Null value is returned.')
+  document.querySelector('.card').innerHTML=`User clicked Cancel, Hence, the response is ${input}`
+
+} else if(input.trim()==="")
+{
+  console.log('User is trying to enter empty string')
+  alert("Entering a empty string is not allowed")
+
+  do
+  {
+
+      input= prompt("Please enter a correct label")
+
+      
+      if(input===null)
+      {
+        console.log('On clicking CANCEL button, Null value is returned.')
+        document.querySelector('.card').innerHTML=`User clicked Cancel, Hence, the response is ${input}`
+
+      } 
+    else if(input.trim()==="")
+      {
+        console.log('User is trying to enter empty string')
+        alert("Entering a empty string is not allowed")
+        //document.querySelector('.card').innerHTML=`User tried entering empty string, Hence, the response is ${input}`
+      }
+  }while(input.trim()==="")
+
+  document.querySelector('.card').innerHTML=`User finally entered string value, Hence, the response is ${input}`
+} else
+    {
+      console.log('User input is: '+ input)
+        //document.querySelector('.card').innerHTML=input;
+
+       const addCard= document.createElement('section');
+       addCard.className='card'
+       addCard.textContent=input.trim();
+       card_container.appendChild(addCard)
+    }
