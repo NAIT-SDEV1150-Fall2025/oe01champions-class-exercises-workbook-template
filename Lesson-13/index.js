96f104817ee848a4cdb2b00448090aaefce75877
rohitt-nait <rohitt@nait.ca>
2025-10-24 18:52:51 -0600

    Intro to Package Manager

---
+++ Lesson-13/index.js
@@ -0,0 +1,9 @@
+import dayjs from 'dayjs';
+
+const now = dayjs();
+
+console.log('Current Date is: ', now.format('YYYY-MM-DD'));
+
+console.log('Current Time is: ', now.format('HH:mm:ss'));
+
+//Execute the index.js and see the results.
