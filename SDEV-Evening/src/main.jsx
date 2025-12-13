8d58868033bd55124e7e1ce89b78afd357922482
rohitt-nait <rohitt@nait.ca>
2025-12-12 19:54:57 -0700

    about react

---
+++ SDEV-Evening/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {App, Greetings} from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Greetings message='Hello User, Lets Learn React'/>
+  </StrictMode>,
+)
